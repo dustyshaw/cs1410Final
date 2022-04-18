05b2acfb8a70c378b949911a0dd4203c8d1ec009
Dusty Shaw <shustydaw@gmail.com>
2022-04-18 10:10:10 -0600

    items.json properly updating now

--- lib/AudioBook.cs
+++ lib/AudioBook.cs
@@ -1,27 +1,27 @@
-namespace MyLibrary.lib;
+// namespace MyLibrary.lib;
 
-public abstract class AudioBook : Book
-{
-    public AudioBook(string _CallNumber, string _Title, Int64 _ISBN, string _Author, Int64 _Barcode) : base(_CallNumber, _Title, _ISBN, _Author, _Barcode)
-    {
+// public abstract class AudioBook : Book
+// {
+//     public AudioBook(string _CallNumber, string _Title, Int64 _ISBN, string _Author, Int64 _Barcode) : base(_CallNumber, _Title, _ISBN, _Author, _Barcode)
+//     {
 
-    }
-    public string CheckOut(ILibraryItem item, Account account, List<ILibraryItem> holdList)
-    {
-        var bookitem = (AudioBook)item;
-        bookitem.Availability = ItemAvailability.CheckedOut;
-        DateTime today = DateTime.Today;
-        var DueDate = today;
-        return ("Item successfully checked out to: " + account.FirstName + " " + account.LastName);
-    }
-    public string CheckIn(ILibraryItem item)
-    {
-        var bookitem = (Book)item;
-        bookitem.Availability = ItemAvailability.CheckedIn;
-        return ("Item successfully checked in.");
-    }
-    public virtual string GetDetails()
-    {
-        return $"\n \n CallNumber: {CallNumber} Title: {Title} \n Author: {Author} \n ISBN: {ISBN} \n Item Type: {Type} \n Availabilty: {Availability}";
-    }
-}
+//     }
+//     public string CheckOut(ILibraryItem item, Account account, List<ILibraryItem> holdList)
+//     {
+//         var bookitem = (AudioBook)item;
+//         bookitem.Availability = ItemAvailability.CheckedOut;
+//         DateTime today = DateTime.Today;
+//         var DueDate = today;
+//         return ("Item successfully checked out to: " + account.FirstName + " " + account.LastName);
+//     }
+//     public string CheckIn(ILibraryItem item)
+//     {
+//         var bookitem = (Book)item;
+//         bookitem.Availability = ItemAvailability.CheckedIn;
+//         return ("Item successfully checked in.");
+//     }
+//     public virtual string GetDetails()
+//     {
+//         return $"\n \n CallNumber: {CallNumber} Title: {Title} \n Author: {Author} \n ISBN: {ISBN} \n Item Type: {Type} \n Availabilty: {Availability}";
+//     }
+// }
--- lib/EAudioBook.cs
+++ lib/EAudioBook.cs
@@ -1,14 +1,14 @@
-namespace MyLibrary.lib;
+// namespace MyLibrary.lib;
 
-public class EAudioBook : AudioBook
-{
-    public decimal RunTime;
-    public EAudioBook(string _CallNumber, string _Title, Int64 _ISBN, string _Author, Int64 _Barcode, decimal _RunTime) : base(_CallNumber, _Title, _ISBN, _Author, _Barcode)
-    {
-        this.RunTime = _RunTime;
-    }
-    public override string GetDetails()
-    {
-        return $"\n \n CallNumber: {CallNumber} Title: {Title} \n Author: {Author} \n ISBN: {ISBN} \n Item Type: {Type} \n Availabilty: {Availability} \n RunTime: {RunTime}";
-    }
-}
+// public class EAudioBook : AudioBook
+// {
+//     public decimal RunTime;
+//     public EAudioBook(string _CallNumber, string _Title, Int64 _ISBN, string _Author, Int64 _Barcode, decimal _RunTime) : base(_CallNumber, _Title, _ISBN, _Author, _Barcode)
+//     {
+//         this.RunTime = _RunTime;
+//     }
+//     public override string GetDetails()
+//     {
+//         return $"\n \n CallNumber: {CallNumber} Title: {Title} \n Author: {Author} \n ISBN: {ISBN} \n Item Type: {Type} \n Availabilty: {Availability} \n RunTime: {RunTime}";
+//     }
+// }
